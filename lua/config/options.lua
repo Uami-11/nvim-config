00b725a76fb3c95b1e179ee6b8b826fbf8bd3658
Uami-11 <nirwan.m.9618@gmail.com>
2026-03-22 12:17:27 +0545

    added some stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,12 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
-  callback = function()
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-  end,
+    pattern = "lua",
+    callback = function()
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
+    end,
 })
+local opt = vim.opt
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true -- Tells Vim to insert spaces instead of a Tab character.
