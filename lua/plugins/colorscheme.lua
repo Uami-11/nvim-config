0a77b72aeab5b66eaed3e3bc69fae0bc9d8b55c5
Uami-11 <nirwan.m.9618@gmail.com>
2026-04-09 08:02:02 +0545

    fix: all transparent now

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+    },
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true, -- Enable transparency
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
