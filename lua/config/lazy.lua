75b35d276587bd3932435e746de2a2c4d1b4529a
Uami-11 <nirwan.m.9618@gmail.com>
2026-04-15 13:44:53 +0545

    add: stop using the arrow keys for suggestions

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,58 +1,58 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- ONLY ONE SETUP CALL ALLOWED
 require("lazy").setup({
-    spec = {
-        -- 1. Import LazyVim core
-        { "LazyVim/LazyVim",                                import = "lazyvim.plugins" },
+  spec = {
+    -- 1. Import LazyVim core
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-        -- 2. Import Language Extras
-        { import = "lazyvim.plugins.extras.lang.python" },
-        { import = "lazyvim.plugins.extras.lang.go" },
-        { import = "lazyvim.plugins.extras.lang.dotnet" },
-        { import = "lazyvim.plugins.extras.lang.clangd" },
-        { import = "lazyvim.plugins.extras.lang.java" },
-        { import = "lazyvim.plugins.extras.lang.typescript" },
-        { import = "lazyvim.plugins.extras.lang.tailwind" },
-        { import = "lazyvim.plugins.extras.lang.json" },
+    -- 2. Import Language Extras
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.dotnet" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.json" },
 
-        -- 3. Import your custom plugins from lua/plugins/
-        { import = "plugins" },
+    -- 3. Import your custom plugins from lua/plugins/
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
     },
-    defaults = {
-        lazy = false,
-        version = false,
-    },
-    install = { colorscheme = { "tokyonight", "habamax" } },
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                "gzip",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
+  },
 })
 
 --
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,6 +2,22 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
+      -- 1. Ensure the first item is automatically highlighted
+      completion = {
+        list = {
+          selection = { preselect = true, auto_insert = true },
+        },
+      },
+      -- 2. Map Space (or any key) to accept the selection
+      keymap = {
+        preset = "default",
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        -- Use "select_and_accept" so it picks the preselected item
+        ["<Space>"] = { "select_and_accept", "fallback" },
+        -- Or use Enter if you prefer
+        ["<CR>"] = { "accept", "fallback" },
+      },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
